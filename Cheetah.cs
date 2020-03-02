85045bb27f48b5f08c1ea85727bb5c4951a1fece
apple <43368992+HardijsR@users.noreply.github.com>
2020-03-02 15:42:35 +0000

    CHEETAH

---
+++ Cheetah.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FelineCalculator
+{
+    class Cheetah : ICanClimb, ICanSwim
+    {
+        public void Climb()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
